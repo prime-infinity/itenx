cc92fa759ce764c626826ad2d787b7ee845036b6
prime-infinity <soe.a.prime@gmail.com>
2021-07-11 11:38:37 +0100

    started login

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import { Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About'
-import Lists from './pages/Lists'
-import Header from './components/Header'
+import Login from './pages/Login';
 
 function App() {
 
   return (
    <div>
 
-     <Header />
-
      <Switch>
-      <Route path='/' exact>
-        <Home />
-      </Route>
-      <Route path='/about'>
-        <About />
-      </Route>
-      <Route path='/list'>
-        <Lists />
-      </Route>
-    </Switch>
+        <Route path='/login'>
+          <Login />
+        </Route>
+     </Switch>
+     
    </div>
   ); 
 }
--- src/app2.js
+++ src/app2.js
@@ -67,3 +67,32 @@ function App() {
 }
 
 export default App;
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About'
+import Lists from './pages/Lists'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+   <div>
+
+     <Header />
+
+     <Switch>
+      <Route path='/' exact>
+        <Home />
+      </Route>
+      <Route path='/about'>
+        <About />
+      </Route>
+      <Route path='/list'>
+        <Lists />
+      </Route>
+    </Switch>
+   </div>
+  ); 
+}
+
+export default App;
