206fda8f9845f1852b772d82128a9fa0bc209bdb
prime-infinity <soe.a.prime@gmail.com>
2021-07-21 01:01:49 +0100

    playing with dynamic classes

--- src/app2.js
+++ src/app2.js
@@ -145,4 +145,8 @@ return(authCtx.auth.name ?
 
             {authCtx.auth.name ? 'logged in' : 'not logged in'}
 
-            
+            <div className={`col-md-3 mr-5 ml-5 col-12 mb-4 mt-4 blackBorder ${isShown ? classes.stuffPopoutForLap:""} ${addingId ? classes.stuffPopout:""}`} onClick={showStuffOptions} onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
+            <div className="row">
+                <img className="img-fluid" width="100%" src={` /stuffimages/${props.stuffprop.image }`} alt="imasge"/>
+            </div>
+        </div>
--- src/components/stuffs/ParticularStuff.js
+++ src/components/stuffs/ParticularStuff.js
@@ -1,13 +1,23 @@
-//import classes from '../stuffs/Stuffs.module.css'
+import classes from '../stuffs/Stuffs.module.css'
+import { useState } from 'react';
 
 function ParticularStuff(props){
 
+    const [addingId, addClassToId] = useState();
+
     function showStuffOptions(){
         //console.log(props.stuffprop.id)
+        addClassToId(props.stuffprop.id)
+        
+
+        if(addingId === props.stuffprop.id){
+            addClassToId(0)
+        }
+       
     }
  
     return (
-        <div className={`col-md-3 mr-5 ml-5 col-12 mb-4 mt-4 blackBorder`} id={`stuffdiv${props.stuffprop.id}`} onClick={showStuffOptions}>
+        <div className={`col-md-3 mr-5 ml-5 col-12 mb-4 mt-4 blackBorder ${addingId === props.stuffprop.id ? classes.stuffPopout:""}`} id={`stuffdiv${props.stuffprop.id}`} onClick={showStuffOptions}>
             <div className="row">
                 <img className="img-fluid" width="100%" src={` /stuffimages/${props.stuffprop.image }`} alt="imasge"/>
             </div>
